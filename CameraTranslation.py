803dc5cb8845e57b0961111fc9211b04785e83c1
Dylan Freeman <31598304+bg98rz@users.noreply.github.com>
2019-09-21 21:18:01 +0100

    Update CameraTranslation.py

--- CameraTranslation.py
+++ CameraTranslation.py
@@ -5,24 +5,26 @@ import math
 from numpy.fft import fft2, ifft2, fftshift
 
 class CameraTranslation(object):
-    '''
+    """
     Class for calculating translational shift betwen two frames
-    '''
+    """
+    
     version = '2019.0.1'
     
-    def __init__(self, initial_frame)
-    'initializer method'
-        self.initial_frame = np.float32(cv2.cvtColor(initial_frame, cv2.COLOR_BGR2GRAY))    # convert to required type
+    def __init__(self, initial_frame):
+        """initializer method"""
+        self.initial_frame = np.float32(
+            cv2.cvtColor(initial_frame, cv2.COLOR_BGR2GRAY))    # convert to required type
         
     def detect_phase_shift(self, curr_frame):
-        'opencv implementation - returns detected sub-pixel phase-shift between two frames'
-        curr_frame = np.float32(cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY))    # convert to required type
-         #calculate phase-correlation between current and previous frame
-        shift = cv2.phaseCorrelate(self.initial_frame, curr_frame)
+        """opencv implementation - returns detected sub-pixel phase-shift between two frames"""
+        curr_frame = np.float32(
+            cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY))           
+        shift = cv2.phaseCorrelate(self.initial_frame, curr_frame)      #get phase-shift 
         return shift
 
     def fft_phase_shift(self, im0, im1):
-        'stand-alone implementation - returns x, y translation between two frames'
+        """stand-alone implementation - returns x, y translation between two frames"""
         shape = im0.shape
         f0 = fft2(im0)
         f1 = fft2(im1)
